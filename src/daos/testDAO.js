f8de52160e4fbb52c0dd754acf7f399a4464c129
Mike <Mike@macbook-pro-4.home>
2019-07-02 00:30:29 +0200

    testDAO

---
+++ src/daos/testDAO.js
@@ -0,0 +1,93 @@
+var mongoose = require("mongoose");
+var Phones = require("../persisters/phones.js");
+
+
+const demoLookup = function (query) {
+  return query.concat([
+    {
+      $lookup: {
+        from: "phones",
+        localField: "desired_model",
+        foreignField: "model",
+        as: "searched_phones"
+      }
+    },
+    {
+      $lookup: {
+        from: "phones",
+        localField: "desired_color",
+        foreignField: "color",
+        as: "phone_by_fav_color"
+      }
+    },
+    {
+      $lookup: {
+        from: "accesories",
+        localField: "desired_body_type",
+        foreignField: "body_type",
+        as: "desired_accesories"
+      }
+    },
+    {
+      $lookup: {
+        from: "network_offers",
+        localField: "chosen_network",
+        foreignField: "network",
+        as: "network_offers"
+      }
+    }
+  ]);
+};
+
+const getAllAdvanced = function (filters, successCbk, errorCbk) {
+  var {
+    phone_type,
+    desired_color,
+    desired_body_type,
+    chosen_network
+  } = filters;
+
+  var matchQuery = {
+    $match: {
+      sales_start_date: { $gte: new Date(sales_start_date) },
+      sales_end_date: { $lte: new Date(sales_end_date) },
+    }
+  };
+
+  if (phone_type !== "null" && phone_type !== "undefined")
+    matchQuery.$match["phone_type"] = phone_type;
+
+  if (desired_color !== "null" && desired_color !== "undefined")
+    matchQuery.$match["desired_color"] = desired_color;
+
+  if (desired_body_type !== "null" && desired_body_type !== "undefined")
+    matchQuery.$match["desired_body_type"] = desired_body_type;
+
+  if (chosen_network !== "null" && chosen_network !== "undefined")
+    matchQuery.$match["chosen_network"] = chosen_network;
+
+  Replacement.aggregate(
+    [{
+      $lookup: {
+        from: "phones",
+        localField: "_id",
+        foreignField: "user_id",
+        as: "phones"
+      }
+    },
+    { $match: { "phones._id": { $exists: true } } }]
+      .concat(doctorLookup([]).concat([
+        matchQuery,
+        { $sort: { start_date: 1 } }
+      ])),
+    function (err, phones) {
+      if (err) return errorCbk(err);
+      return successCbk(phones);
+    }
+  );
+};
+
+
+module.exports = {
+  getAllAdvanced: getAllAdvanced
+}
